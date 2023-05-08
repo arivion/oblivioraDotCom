7263d0533785d850ec90943f256465cbd2777abc
Alex <Alex@TISIPHONE>
2023-05-07 21:52:31 -0400

    Add project files.

---
+++ oblivioraDotCom/ClientApp/src/components/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class Home extends Component {
+  static displayName = Home.name;
+
+  render() {
+    return (
+      <div>
+        <p>A landing page with nothing to link to :(</p>
+      </div>
+    );
+  }
+}
